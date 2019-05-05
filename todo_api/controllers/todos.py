00dfc70ebf339212209aa4d2c9c85f6b2b3b6722
Oltho <tran.thomasteddy@gmail.com>
2019-05-05 18:01:11 +0200

    add todos api call
    update api.yaml with new api call and implement empty function for each operationID

---
+++ todo_api/controllers/todos.py
@@ -0,0 +1,17 @@
+def find_todos():
+    pass
+
+def create_todos():
+    pass
+
+
+def find_todos_by_id():
+    pass
+
+def update_todos_by_id():
+    pass
+
+
+def delete_todos_by_id():
+    pass
+
