184e07b8c041d68fd6f6711dfd0f62af29f5ed2e
Oltho <tran.thomasteddy@gmail.com>
2019-05-06 23:18:06 +0200

    fix column name

--- todo_api/models/Todos.py
+++ todo_api/models/Todos.py
@@ -5,6 +5,6 @@ class Todos(db.Model):
     __tablename__ = "todos"
 
     id = db.Column(db.Integer, primary_key=True)
-    tittle = db.Column(db.String(128), nullable=False)
+    title = db.Column(db.String(128), nullable=False)
     description = db.Column(db.String(), nullable=True)
     done = db.Column(db.Boolean, nullable=False)
