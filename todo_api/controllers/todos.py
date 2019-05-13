967e81548c15ffd1ce26bedddb171b60d1cdb9eb
Oltho <tran.thomasteddy@gmail.com>
2019-05-14 00:34:33 +0200

    update controller to pass test for find_todos

--- todo_api/controllers/todos.py
+++ todo_api/controllers/todos.py
@@ -1,12 +1,25 @@
+from todo_api.models import db
+from todo_api.models.Todos import Todos, TodosSchema
+
+
 def find_todos():
+    """ Fetch and return the list of all todo task """
+    todos = Todos.query.order_by(Todos.id).all()
+    todo_schema = TodosSchema(many=True)
+    todos_docs = todo_schema.dump(todos).data
+
+    result = {'todos': todos_docs}
+
+    return result, 200
+
+
+def create_todos(body):
     pass
 
-def create_todos():
+
+def find_todos_by_id(id):
     pass
 
-
-def find_todos_by_id():
-    pass
 
 def update_todos_by_id():
     pass
@@ -14,4 +27,3 @@ def update_todos_by_id():
 
 def delete_todos_by_id():
     pass
-
