c527ef09b46e32175f7104fb692e329f023cd526
Oltho <tran.thomasteddy@gmail.com>
2019-05-14 00:14:58 +0200

    add fixture data for todo

---
+++ tests/fixtures/todos.py
@@ -0,0 +1,29 @@
+todo_fixture = {
+    'table': 'todos',
+    'records': [
+        {
+            'id': 1,
+            'title': 'Todo 1',
+            'description': 'Description 1',
+            'done': False
+        },
+        {
+            'id': 2,
+            'title': 'Todo 2',
+            'description': 'Description 2',
+            'done': True
+        },
+        {
+            'id': 3,
+            'title': 'Todo 3',
+            'description': None,
+            'done': True
+        },
+        {
+            'id': 4,
+            'title': 'Todo 4',
+            'description': None,
+            'done': False
+        }
+    ]
+}
